01a03b589f4d04b23007b9d8ed9b005caea87e94
ukslee <uks78n@gmail.com>
2019-01-24 15:50:49 +0900

    fix for rubocop convention

--- test/integration/default/controls/tf_output.rb
+++ test/integration/default/controls/tf_output.rb
@@ -1,10 +1,14 @@
-control "terraform_output" do
-  desc "Verifies that the Terraform state file contains proper output result"
+control 'terraform_output' do
+  desc 'Verifies that the Terraform state file contains proper output result'
 
-  tfstate_json=json(command('find $(pwd) -name "terraform.tfstate"').stdout.gsub("\n",""))
-  sample_mod=tfstate_json.modules.find { |mod| mod["path"]==["root","sample_mod"]}
+  tfstate_json = json(
+    command('find $(pwd) -name "terraform.tfstate"')
+    .stdout.delete("\n")
+  )
+  sample_mod = tfstate_json.modules
+                           .find { |mod| mod['path'] == %w[root sample_mod] }
 
-  describe sample_mod["outputs"]["sample_output"]["value"] do
-    it { should eq 'sample output'}
+  describe sample_mod['outputs']['sample_output']['value'] do
+    it { should eq 'sample output' }
   end
 end
