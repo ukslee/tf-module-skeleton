807149907f13fa4c61f02152031ce0d6dbd84987
ukslee <uks78n@gmail.com>
2019-01-25 12:58:17 +0900

    change example code ref

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,5 +1,5 @@
 module "sample_mod" {
-  source = "github.com/ukslee/tf-module-skeleton.git?ref=master"
+  source = "github.com/ukslee/tf-module-skeleton.git?ref=v0.1.0"
 
   sample_variable = "sample_var"
 }
